80297b50c44a3dd11cbf93d0d2357fadc276b2af
José Joaquín Garci Peña <jojogape@outlook.es>
2023-10-16 12:57:32 +0200

    Reestructuro Calculadora

--- MyApplication/app/src/main/java/com/example/myapplication/Calculadora.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Calculadora.java
@@ -3,20 +3,41 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Calculadora {
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        setContentView(R.layout.activity_calc);
-//        Button btnSaludar = findViewById(R.id.btnSaludar);
-//        Button btnDespedir = findViewById(R.id.btnDespedir);
-//
-//        btnSaludar.setOnClickListener(this);
-//        btnDespedir.setOnClickListener(this);
-//    }
+public class Calculadora extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnIgual;
+    private TextView txtExp;
+    private String cadena;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        btnIgual = findViewById(R.id.btnIgual);
+
+        btnIgual.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnIgual){
+            try {
+                ExpressionBuilder expressionBuilder = new ExpressionBuilder(cadena);
+                Expression exp = expressionBuilder.build();
+                double res = exp.evaluate();
+                txtExp.setText(""+res);
+                cadena = ""+res;
+            } catch (Exception e) {
+                txtExp.setText("ERR");
+                cadena="";
+            }
+        }
+    }
 }
--- MyApplication/settings.gradle.kts
+++ MyApplication/settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "My Application"
+rootProject.name = "Calculadora"
 include(":app")
