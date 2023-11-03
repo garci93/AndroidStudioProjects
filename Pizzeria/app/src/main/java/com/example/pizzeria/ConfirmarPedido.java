041b22cb144baabcf94895eef26ab3231e8dc419
José Joaquín Garci Peña <jojogape@outlook.es>
2023-11-03 14:25:22 +0100

    clases y vistas Pizzeria bien

---
+++ Pizzeria/app/src/main/java/com/example/pizzeria/ConfirmarPedido.java
@@ -0,0 +1,15 @@
+package com.example.pizzeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmarPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmar_pedido);
+    }
+}
