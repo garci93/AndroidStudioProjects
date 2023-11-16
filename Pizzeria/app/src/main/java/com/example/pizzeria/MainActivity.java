68507ec69597866400a35338b584d883d7b7a67b
José Joaquín Garci Peña <jojogape@outlook.es>
2023-11-16 11:12:06 +0100

    iniciar sesiom

--- Pizzeria/app/src/main/java/com/example/pizzeria/MainActivity.java
+++ Pizzeria/app/src/main/java/com/example/pizzeria/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.pizzeria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnIniciarSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Inicio.class);
+                startActivity(i);
+            }
+        });
     }
 }
