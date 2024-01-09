5ae89233c4cdeadcee0262cf0148e80f617c9750
José Joaquín Garci Peña <jojogape@outlook.es>
2024-01-09 14:34:31 +0100

    testjuego

---
+++ TestJuego/app/src/main/java/com/example/testjuego/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.testjuego;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Paint paint;
+    class RenderView extends View {
+        public RenderView(Context context) {
+            super(context);
+
+            paint = new Paint();
+        }
+
+        protected void onDraw(Canvas canvas){
+            canvas.drawRGB(255, 255, 255);
+            paint.setColor(Color.RED);
+            canvas.drawLine(0,0,canvas.getWidth()-1, canvas.getHeight()-1, paint);
+
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(0xff00ff00);
+            canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight()/2,40,paint);
+
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(0x770000ff);
+            canvas.drawRect(100,100,200,200,paint);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new RenderView(this));
+    }
+}
