435060f68cec511b02e8c3c9d1d2654728f181e8
José Joaquín Garci Peña <jojogape@outlook.es>
2024-02-22 11:11:17 +0100

    sprites para el juego

--- JuegoConSprite/app/src/main/java/com/example/juegoconsprite/MainActivity.java
+++ JuegoConSprite/app/src/main/java/com/example/juegoconsprite/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setRequestedOrientation((ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE));
+        setRequestedOrientation((ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT));
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
