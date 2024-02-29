5787267ac9e4b25b99ca6ae1884115470042babd
José Joaquín Garci Peña <jojogape@outlook.es>
2024-02-29 12:27:13 +0100

    juego aprobado :)

--- JuegoConSprite/app/src/main/java/com/example/juegoconsprite/MainActivity.java
+++ JuegoConSprite/app/src/main/java/com/example/juegoconsprite/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        ImageView backgroundImageView = findViewById(R.id.imageView);
 
         super.onCreate(savedInstanceState);
         setContentView(new MoverFiguras(this));
